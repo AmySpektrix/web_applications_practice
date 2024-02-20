b6e9a6ffbf7d610932f18c92d20cf66245c4a077
Amy Brown <amy.brown@spektrix.com>
2024-02-20 13:14:25 +0000

    interim update

--- hello_web_project/app.py
+++ hello_web_project/app.py
@@ -5,7 +5,31 @@ from flask import Flask, request
 app = Flask(__name__)
 
 # == Your Routes Here ==
+@app.route('/', methods=['GET'])
+def index():
+    return "Blank index"
 
+@app.route('/hello', methods=['GET'])
+def hello():
+    name = request.args['name']
+    return f"Hello {name}!"
+
+@app.route('/goodbye', methods=['POST'])
+def goodbye():
+    name = request.form['name']
+    goodbye = request.form['goodbye']
+    return f"{goodbye} {name}!"
+
+@app.route('/submit', methods=['POST'])
+def submit_message():
+    name = request.form['name']
+    message = request.form ['message']
+    return f"Thanks {name}, you sent this message: {message}"
+    
+
+@app.route('/listings', methods=['GET'])
+def all():
+    return f"Hello {name}!"
 
 # These lines start the server if you run this file directly
 # They also start the server configured to use the test database
